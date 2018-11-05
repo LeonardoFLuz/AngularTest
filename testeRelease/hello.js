a47c650688115b76f8caba5f6e6479607d160e74
Leonardo <44617350+LeonardoFLuz@users.noreply.github.com>
2018-11-05 13:36:17 -0200

    Delete hello.js

--- testeRelease/hello.js
+++
@@ -1,8 +0,0 @@
-var app = angular.module('ExampleApp', []);
-
-app.controller('ExampleController', function($scope, $http) {
-    $http.get('https://ghibliapi.herokuapp.com/films/').
-        then(function(response) {
-            $scope.exampleData = response.data;
-        });
-});
